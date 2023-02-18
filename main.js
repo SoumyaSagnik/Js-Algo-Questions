c1933c9c6cfa12d10d013be611fd0790cae7694b
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-02-18 19:03:31 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// ! Remove duplicates from an array
-function removeDuplicates(arr) {
-  const b = [...new Set(arr)];
-  console.log(b);
-}
-removeDuplicates([1, 2, 1, 1, 2, 1, 3, 5, 9, 3, 1]);
-// output: [1, 2, 3, 5, 9]
-
-console.log("---------------------------------------------------------------");
